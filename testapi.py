630a1416a7666faf254d5ae7e55a74dd9f6c8224
YOSHI-Engineer <43871157+Yoshi-enginner@users.noreply.github.com>
2023-03-21 03:03:59 +0900

    Update testapi.py

--- testapi.py
+++ testapi.py
@@ -1,24 +1,26 @@
 from flask import Flask, request, send_file
 import qrcode
-import io
+from io import BytesIO
 
 app = Flask(__name__)
 
-@app.route('/qrcode', methods=['GET'])
-def generate_qr_code():
-    # Get the URL from the query string
+@app.route('/generate_qr', methods=['GET'])
+def generate_qr():
     url = request.args.get('url')
+    if url:
+        qr = qrcode.QRCode(version=1, box_size=10, border=5)
+        qr.add_data(url)
+        qr.make(fit=True)
+        img = qr.make_image(fill_color='black', back_color='white')
+        file_object = BytesIO()
+        img.save(file_object, 'PNG')
+        file_object.seek(0)
+        return send_file(file_object, mimetype='image/PNG', as_attachment=True, attachment_filename='qr_code.png')
+    else:
+        return 'Invalid URL'
 
-    # Generate a QR code image
-    img = qrcode.make(url)
-
-    # Save the image to a byte buffer
-    img_buffer = io.BytesIO()
-    img.save(img_buffer, format='PNG')
-    img_buffer.seek(0)
-
-    # Send the image back as a response
-    return send_file(img_buffer, mimetype='image/png')
+if __name__ == '__main__':
+    app.run(debug=True)
 
 
 
