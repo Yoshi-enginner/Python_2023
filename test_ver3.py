51c908a9f1c5f0467173d522d05d2cf698b78ab1
YOSHI-Engineer <43871157+Yoshi-enginner@users.noreply.github.com>
2023-03-24 15:06:37 +0900

    Add files via upload

---
+++ test_ver3.py
@@ -0,0 +1,17 @@
+import requests
+
+words = 30
+paragraphs = 1
+formats = 'text'
+response = requests.get(f"https://alexnormand-dino-ipsum.p.rapidapi.com/?format={formats}&words={words}&paragraphs={paragraphs}",
+ headers={
+   "X-RapidAPI-Host": "alexnormand-dino-ipsum.p.rapidapi.com",
+   "X-RapidAPI-Key": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+ }
+)
+print(response.text)
+
+if response:
+  print('Request is successful.')
+else:
+  print('Request returned an error.')
